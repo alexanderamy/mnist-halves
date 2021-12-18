e0b6b83122f8acb1903b9e37954edd04cfd15525
Alexander Amy <aaa453@cornell.edu>
2021-12-17 20:23:16 -0500

    added scheduler

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from objective import LTot
 
-def train(model, optimizer, train_loader, test_loader, device, epochs=1):
+def train(model, optimizer, scheduler, train_loader, test_loader, device, epochs=1):
     for epoch in range(epochs):
         model.train()
         train_loss = 0
@@ -21,6 +21,7 @@ def train(model, optimizer, train_loader, test_loader, device, epochs=1):
             loss = LTot(*model(X, Y))  
             test_loss += loss.data
 
+        scheduler.step()
         train_loss = train_loss / len(train_loader)
         test_loss = test_loss / len(test_loader)
         print(f'epoch: {epoch} | train: {train_loss.item():.3f} | test: {test_loss.item():.3f}')
